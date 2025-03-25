dcce9a57db48f399ddab071240844fb353977f90
Sidnei Farias <sidneijr.77@gmail.com>
2025-03-24 23:42:38 -0300

    feat: add react-scan

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { scan } from 'react-scan'
+
+scan({
+  enabled: true
+})
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
